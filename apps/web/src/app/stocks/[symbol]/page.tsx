c400732ffaf335060fdab855764798154f7da6ba
web-inte <thamer.web@hotmail.com>
2026-07-12 10:46:26 +0000

    fix stock page

--- apps/web/src/app/stocks/[symbol]/page.tsx
+++ apps/web/src/app/stocks/[symbol]/page.tsx
@@ -14,6 +14,10 @@ type StockPageProps = {
 };
 
 function getBaseUrl() {
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
